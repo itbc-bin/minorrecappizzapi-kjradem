2db7d59f9ba04a12e84739eb0e877118648ee92e
kjradem <koenrademaker@outlook.com>
2018-09-04 16:39:23 +0200

    added app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+pizzaDB = [
+            {'name': 'Margaritha'},
+            {'name': 'Hawaii'},
+            {'name': 'Salami'}
+          ]
+
+
+@app.route("/", methods=['GET'])
+def getPizza():
+    return jsonify({'pizzaDB':pizzaDB})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
