e43b6de9f6234b88fb081e4c3fd55e89b8cb51a4
kjradem <koenrademaker@outlook.com>
2018-09-05 14:14:15 +0200

    added new, somewhat delicous options

--- app.py
+++ app.py
@@ -3,16 +3,24 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 pizzaDB = [
-            {'name': 'Margaritha'},
-            {'name': 'Hawaii'},
-            {'name': 'Hawaii met extra ananas voor Sjors'},
-            {'name': 'Salami'}
+            {'name': 'Margaritha', 'plebian': True},
+            {'name': 'Hawaii', 'acceptabel': True},
+            {'name': 'Hawaii met extra ananas voor Sjors', 'acceptabel': True},
+            {'name': 'Salami', 'Kosher': False},
+            {'name': 'Quattro Formaggi', 'si si': True},
+            {'name': 'Chocolade met extra Nutella', 'melts': True}
           ]
 
 
 @app.route("/", methods=['GET'])
-def getPizza():
-    return jsonify({'pizzaDB':pizzaDB})
+def get_pizza():
+    return jsonify({'pizzaDB': pizzaDB})
+
+
+@app.route('/<string:name>', methods=['GET'])
+def get_one_pizza(name):
+    result = [pizza for pizza in pizzaDB if pizza['name'] == name]
+    return jsonify({'pizzaDB': result})
 
 
 if __name__ == '__main__':
